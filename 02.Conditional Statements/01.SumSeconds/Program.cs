e1e558a4ba1baef7328c9d455ac7a1a7e241756a
Krisztian Fodor <63681999+KaiserDMC@users.noreply.github.com>
2025-07-26 09:21:44 +0300

    ex2-solutions

---
+++ 02.Conditional Statements/01.SumSeconds/Program.cs
@@ -0,0 +1,16 @@
+﻿int firstRunnerTime = int.Parse(Console.ReadLine());
+int secondRunnerTime = int.Parse(Console.ReadLine());
+int thirdRunnerTime = int.Parse(Console.ReadLine());
+
+int totalRunnerTime = firstRunnerTime + secondRunnerTime + thirdRunnerTime;
+int totalTimeMinutes = totalRunnerTime / 60;
+int totalTimeSeconds = totalRunnerTime % 60;
+
+if (totalTimeSeconds < 10)
+{
+    Console.WriteLine($"{totalTimeMinutes}:0{totalTimeSeconds}");
+}
+else
+{
+    Console.WriteLine($"{totalTimeMinutes}:{totalTimeSeconds}");
+}
---
+++ 02.Conditional Statements/05.GodzillaVsKong/Program.cs
@@ -0,0 +1,24 @@
+﻿double movieBudget = double.Parse(Console.ReadLine());
+int actors = int.Parse(Console.ReadLine());
+double clothesPricePerActor = double.Parse(Console.ReadLine());
+
+double decors = movieBudget * 0.1;
+
+if (actors > 150)
+{
+    clothesPricePerActor = clothesPricePerActor - clothesPricePerActor * 0.1;
+}
+
+double totalClothesPrice = clothesPricePerActor * actors;
+double budgetDifference = movieBudget - (totalClothesPrice + decors);
+
+if (budgetDifference >= 0)
+{
+    Console.WriteLine("Action!");
+    Console.WriteLine($"Wingard starts filming with {budgetDifference:f2} leva left.");
+}
+else
+{
+    Console.WriteLine("Not enough money!");
+    Console.WriteLine($"Wingard needs {Math.Abs(budgetDifference):f2} leva more.");
+}
